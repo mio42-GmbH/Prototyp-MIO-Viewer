b23f3ecd44250864b1f525000c798cb48a495fd8
dkipping <dennis.kipping@mio42.de>
2023-03-29 10:19:16 +0200

    rebuild storybook

--- react_prototype/.storybook/mio42Theme.js
+++ react_prototype/.storybook/mio42Theme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 export const mio42Theme = create({
     base: 'light',
     brandTitle: 'Der Digitale Impfpass',
-    brandUrl: 'https://mio42-gmbh.github.io/PMI-Dokumentation',
+    brandUrl: 'https://mio42-gmbh.github.io/Prototyp-MIO-Anzeige-Impfpass-JSSPA',
     brandImage: './Logo_Impfpass.svg',
     brandTarget: '_self',
 });
